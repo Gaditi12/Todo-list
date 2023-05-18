8cc92bb54860407e21ffcbb5833f11bdd9383fec
aditigupta <aditi.gupta@deeporion.com>
2023-05-18 17:24:02 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import Form from "./components/Form";
 import { Todos } from "./components/Todos";
-//import TodoList from "./components/TodoList";
+
 const App = () => {
   const [isEdit, setIsEdit] = useState(false);
   const [edit, setEditTodo] = useState("");
@@ -22,7 +22,6 @@ const App = () => {
         <Form isEdit={isEdit} edit={edit} cancelUpdate={cancelUpdate} />
         <Todos handleEditClick={handleEditClick} isEdit={isEdit} />
       </div>
-      {/* <TodoList /> */}
     </>
   );
 };
